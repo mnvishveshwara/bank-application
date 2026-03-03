d139ae09ce927cdf331e92ac4faf0e229e9f0761
vishwa <mnvishveshwara@gmail.com>
2026-03-03 15:00:41 +0530

    Feat: Code clean up

--- src/main/java/com/bankbroker/loanapp/service/storage/FileStorageService.java
+++ src/main/java/com/bankbroker/loanapp/service/storage/FileStorageService.java
@@ -7,11 +7,75 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.*;
 
+//@Slf4j
+//@Service
+//public class FileStorageService {
+//
+//    private static final String BASE_DIR = "C:/app/uploads";
+//
+//    public String store(
+//            MultipartFile file,
+//            String applicationId,
+//            String module,
+//            String fileName) {
+//
+//        if (file == null || file.isEmpty()) {
+//            throw new IllegalArgumentException("File is empty");
+//        }
+//
+//        try {
+//            Path dir = Paths.get(
+//                    BASE_DIR,
+//                    applicationId,
+//                    module
+//            );
+//
+//            Files.createDirectories(dir);
+//
+//            Path targetPath = dir.resolve(fileName);
+//
+//            Files.copy(
+//                    file.getInputStream(),
+//                    targetPath,
+//                    StandardCopyOption.REPLACE_EXISTING
+//            );
+//
+//            return targetPath.toAbsolutePath().toString();
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException("File save failed", e);
+//        }
+//    }
+//
+//    public String getExtension(String filename) {
+//        if (filename == null || !filename.contains(".")) return "";
+//        return filename.substring(filename.lastIndexOf("."));
+//    }
+//
+//    public void delete(String filePath) {
+//
+//        if (filePath == null || filePath.isBlank()) return;
+//
+//        try {
+//
+//            Path path = Paths.get(filePath);
+//
+//            if (Files.exists(path)) {
+//                Files.delete(path);
+//            }
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException("Failed to delete file: " + filePath, e);
+//        }
+//    }
+//}
+
+
 @Slf4j
 @Service
 public class FileStorageService {
 
-    private static final String BASE_DIR = "C:/app/uploads";
+    private static final String BASE_DIR = "uploads";
 
     public String store(
             MultipartFile file,
@@ -59,11 +123,7 @@ public class FileStorageService {
         try {
 
             Path path = Paths.get(filePath);
-
-            if (Files.exists(path)) {
-                Files.delete(path);
-            }
-
+            Files.deleteIfExists(path);
         } catch (IOException e) {
             throw new RuntimeException("Failed to delete file: " + filePath, e);
         }
